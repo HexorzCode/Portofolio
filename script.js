95565b624e2d382ffb766fde49d9002aed2df8a4
HexorzCode <107131696+HexorzCode@users.noreply.github.com>
2023-09-21 15:12:13 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ window.onscroll = function (){
         header.classList.remove('navfix')
     }
 }
+
+const text = document.querySelector(".sec-text");
+
+        const textload = () => {
+            setTimeout(() => {
+                text.textContent = "Daffa Maulana";
+            }, 0);
+            setTimeout(() => {
+                text.textContent = "Collage Student";
+            }, 5000);
+            setTimeout(() => {
+                text.textContent = "Programer";
+            }, 10000);
+            
+        }
+        textload();
+        setInterval(textload, 15000);
